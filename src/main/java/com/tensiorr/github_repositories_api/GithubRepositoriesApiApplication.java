513ceef5660fb1840fed5ed7f9b6f903e9edfbf7
Tensiorr <tesiorowski.marek@gmail.com>
2026-05-27 15:47:01 +0200

    Init commit

---
+++ src/main/java/com/tensiorr/github_repositories_api/GithubRepositoriesApiApplication.java
@@ -0,0 +1,13 @@
+package com.tensiorr.github_repositories_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoriesApiApplication.class, args);
+	}
+
+}
